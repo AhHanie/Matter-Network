3fef39676b1e5a3ccf94e1534c22a240d67c5e70
Ahmed Hanie <flubzd@gmail.com>
2026-04-27 13:28:33 +0300

    fix: construction jobs not hauling from network items

--- source/Patches/HaulAIUtility.cs
+++ source/Patches/HaulAIUtility.cs
@@ -6,12 +6,33 @@ namespace SK_Matter_Network.Patches
 {
     public static class Patch_HaulAIUtility
     {
+        public static bool InTryOpportunisticJob;
+
+        [HarmonyPatch(typeof(Pawn_JobTracker), "TryOpportunisticJob")]
+        public static class TryOpportunisticJob
+        {
+            public static void Prefix()
+            {
+                InTryOpportunisticJob = true;
+            }
+
+            public static void Postfix()
+            {
+                InTryOpportunisticJob = false;
+            }
+        }
+
         [HarmonyPatch(typeof(HaulAIUtility), nameof(HaulAIUtility.PawnCanAutomaticallyHaulFast))]
         public static class PawnCanAutomaticallyHaulFast
         {
             [HarmonyPriority(Priority.First)]
             public static bool Prefix(Pawn p, Thing t, ref bool __result)
             {
+                if (!InTryOpportunisticJob)
+                {
+                    return true;
+                }
+
                 NetworksMapComponent mapComp = p.Map.GetComponent<NetworksMapComponent>();
                 if (mapComp.TryGetItemNetwork(t, out _))
                 {
